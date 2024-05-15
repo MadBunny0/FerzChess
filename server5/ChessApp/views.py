df57cf9736c4be56d94a84766736407e84791bb7
MadBunny0 <uspenskiy.artem@yandex.ru>
2024-05-15 06:45:14 +0300

    initial commit

---
+++ server5/ChessApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def move_piece(request):
+    if request.method == 'POST':
+        # Получаем данные о ходе из тела POST-запроса
+        data = request.POST  # или request.body, в зависимости от формата данных
+
+        # Выполняем логику обработки хода
+        # Например, сохраняем информацию о ходе в базе данных или обновляем игровое состояние
+
+        # Возвращаем успешный JSON-ответ
+        return JsonResponse({'success': True, 'message': 'Ход успешно обработан'})
+    else:
+        # Если запрос не является POST-запросом, возвращаем JSON-ответ с ошибкой
+        return JsonResponse({'success': False, 'message': 'Метод не разрешен'})
